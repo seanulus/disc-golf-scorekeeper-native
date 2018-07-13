6da181c594308287b1cb4ba212190faf7b77b484
Sean McDermott <seanjmac@hotmail.com>
2018-07-13 10:56:26 -0700

    Add boilerplate for new player form component

---
+++ src/components/NewPlayerForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const NewPlayerForm = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+}
+
+export default NewPlayerForm;
