52ed401c14c7cfde7cf35734edee4fddf0dcb4fe
Sean McDermott <seanjmac@hotmail.com>
2018-07-13 09:59:04 -0700

    add styling to background

--- App.js
+++ App.js
@@ -1,28 +1,22 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, ImageBackground} from 'react-native';
 import Header from './src/components/Header';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+// const instructions = Platform.select({
+//   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+//   android:
+//     'Double tap R on your keyboard to reload,\n' +
+//     'Shake or press menu button for dev menu',
+// });
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View>
-        <Header />
+      <View style={styles.container}>
+        <ImageBackground source={require('./src/assets/images/background.jpg')} style={styles.backgroundImage}>
+          <Header />
+        </ImageBackground>
       </View>
     );
   }
@@ -33,12 +27,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  backgroundImage: {
+    flex: 1,
+    width: '100%',
   },
   instructions: {
     textAlign: 'center',
