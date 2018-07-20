2e74a4ebd7961fbf0168127086c1ad2483df0636
Sean McDermott <seanjmac@hotmail.com>
2018-07-20 12:02:11 -0700

    Add boilerplate for player list component

---
+++ src/components/PlayerList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class PlayerList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Player List</Text>
+      </View>
+    );
+  }
+}
+
+export default PlayerList;
